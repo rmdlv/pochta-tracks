5097f844ea1fb20e20bb21418ea10b4738f0d49f
rmdlv <60815271+rmdlv@users.noreply.github.com>
2022-09-18 18:20:30 +0800

    frontend

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from api.api import api_router
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.include_router(api_router)
+app.mount("/", StaticFiles(directory="static"), name="static")
